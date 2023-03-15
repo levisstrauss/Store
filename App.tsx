8e9ea3ec5db348b9f0102f39422f24eab12228ee
levisstrauss <levisstrauss11@yahoo.com>
2023-03-15 01:10:19 -0400

    Build the welcome screen

--- App.tsx
+++ App.tsx
@@ -1,30 +1,15 @@
 import React from 'react';
-
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import {useDeviceOrientation, useImageDimensions } from "@react-native-community/hooks";
+import { SafeAreaView, StyleSheet } from "react-native";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
 
 function App(): JSX.Element {
-
-  // @ts-ignore
-  const {landscape}  = useDeviceOrientation();
-
   return (
-      <SafeAreaView style={styles.container}>
-        <View  style={{
-          backgroundColor: "dodgerblue",
-          width: "100%",
-          height: landscape ? '100%' : '30%',
-        }}>
-
-        </View>
-      </SafeAreaView>
+        <WelcomeScreen />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-   flex:1
-  },
+
 });
 
 export default App;
